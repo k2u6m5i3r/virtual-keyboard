291a7b5c466faac6bf16b56ffded01d3f0ff2d0a
k2u6m5i3r <wunder49@yandex.by>
2023-04-30 19:27:15 +0300

    add building keyboard in js

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,525 @@
+const keys = {
+    Backquote: {
+        keyShiftUpEn: "~",
+        keyUpEn: "`",
+        keyShiftUpRu: "Ё",
+        keyUpRu: "ё",
+        isShift: "true",
+        isCaps: "true",
+    },
+    Digit1: {
+        keyShiftUpEn: "!",
+        keyUpEn: "1",
+        keyShiftUpRu: "!",
+        keyUpRu: "1",
+        isShift: "true",
+        isCaps: "false",
+    },
+    Digit2: {
+        keyShiftUpEn: "@",
+        keyUpEn: "2",
+        keyShiftUpRu: "\"",
+        keyUpRu: "2",
+        isShift: "true",
+        isCaps: "false",
+    },
+    Digit3: {
+        keyShiftUpEn: "#",
+        keyUpEn: "3",
+        keyShiftUpRu: "№",
+        keyUpRu: "3",
+        isShift: "true",
+        isCaps: "false",
+    },
+    Digit4: {
+        keyShiftUpEn: "$",
+        keyUpEn: "4",
+        keyShiftUpRu: ";",
+        keyUpRu: "4",
+        isShift: "true",
+        isCaps: "false",
+    },
+    Digit5: {
+        keyShiftUpEn: "%",
+        keyUpEn: "5",
+        keyShiftUpRu: "%",
+        keyUpRu: "5",
+        isShift: "true",
+        isCaps: "false",
+    },
+    Digit6: {
+        keyShiftUpEn: "^",
+        keyUpEn: "6",
+        keyShiftUpRu: ":",
+        keyUpRu: "6",
+        isShift: "true",
+        isCaps: "false",
+    },
+    Digit7: {
+        keyShiftUpEn: "&",
+        keyUpEn: "7",
+        keyShiftUpRu: "?",
+        keyUpRu: "7",
+        isShift: "true",
+        isCaps: "false",
+    },
+    Digit8: {
+        keyShiftUpEn: "*",
+        keyUpEn: "8",
+        keyShiftUpRu: "*",
+        keyUpRu: "8",
+        isShift: "true",
+        isCaps: "false",
+    },
+    Digit9: {
+        keyShiftUpEn: "(",
+        keyUpEn: "9",
+        keyShiftUpRu: "(",
+        keyUpRu: "9",
+        isShift: "true",
+        isCaps: "false",
+    },
+    Digit0: {
+        keyShiftUpEn: ")",
+        keyUpEn: "0",
+        keyShiftUpRu: ")",
+        keyUpRu: "0",
+        isShift: "true",
+        isCaps: "false",
+    },
+    Minus: {
+        keyShiftUpEn: "_",
+        keyUpEn: "-",
+        keyShiftUpRu: "_",
+        keyUpRu: "-",
+        isShift: "true",
+        isCaps: "false",
+    },
+    Equal: {
+        keyShiftUpEn: "+",
+        keyUpEn: "=",
+        keyShiftUpRu: "+",
+        keyUpRu: "=",
+        isShift: "true",
+        isCaps: "false",
+    },
+    Backspace: {
+        keyShiftUpEn: "Backspace",
+        keyUpEn: "Backspace",
+        keyShiftUpRu: "Backspace",
+        keyUpRu: "Backspace",
+        isShift: "false",
+        isCaps: "false",
+    },
+    Tab: {
+        keyShiftUpEn: "Tab",
+        keyUpEn: "Tab",
+        keyShiftUpRu: "Tab",
+        keyUpRu: "Tab",
+        isShift: "false",
+        isCaps: "false",
+    },
+    KeyQ: {
+        keyShiftUpEn: "Q",
+        keyUpEn: "q",
+        keyShiftUpRu: "Й",
+        keyUpRu: "й",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyW: {
+        keyShiftUpEn: "W",
+        keyUpEn: "w",
+        keyShiftUpRu: "Ц",
+        keyUpRu: "ц",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyE: {
+        keyShiftUpEn: "E",
+        keyUpEn: "e",
+        keyShiftUpRu: "У",
+        keyUpRu: "у",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyR: {
+        keyShiftUpEn: "R",
+        keyUpEn: "r",
+        keyShiftUpRu: "К",
+        keyUpRu: "к",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyT: {
+        keyShiftUpEn: "T",
+        keyUpEn: "t",
+        keyShiftUpRu: "Е",
+        keyUpRu: "е",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyY: {
+        keyShiftUpEn: "Y",
+        keyUpEn: "y",
+        keyShiftUpRu: "Н",
+        keyUpRu: "н",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyU: {
+        keyShiftUpEn: "U",
+        keyUpEn: "u",
+        keyShiftUpRu: "Г",
+        keyUpRu: "г",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyI: {
+        keyShiftUpEn: "I",
+        keyUpEn: "i",
+        keyShiftUpRu: "Ш",
+        keyUpRu: "ш",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyO: {
+        keyShiftUpEn: "O",
+        keyUpEn: "o",
+        keyShiftUpRu: "Щ",
+        keyUpRu: "щ",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyP: {
+        keyShiftUpEn: "P",
+        keyUpEn: "p",
+        keyShiftUpRu: "З",
+        keyUpRu: "з",
+        isShift: "true",
+        isCaps: "true",
+    },
+    BracketLeft: {
+        keyShiftUpEn: "{",
+        keyUpEn: "[",
+        keyShiftUpRu: "Х",
+        keyUpRu: "х",
+        isShift: "true",
+        isCaps: "true",
+    },
+    BracketRight: {
+        keyShiftUpEn: "}",
+        keyUpEn: "]",
+        keyShiftUpRu: "Ъ",
+        keyUpRu: "ъ",
+        isShift: "true",
+        isCaps: "true",
+    },
+    Backslash: {
+        keyShiftUpEn: "|",
+        keyUpEn: "\\",
+        keyShiftUpRu: "/",
+        keyUpRu: "\\",
+        isShift: "true",
+        isCaps: "true",
+    },
+    Delete: {
+        keyShiftUpEn: "Del",
+        keyUpEn: "Del",
+        keyShiftUpRu: "Del",
+        keyUpRu: "Del",
+        isShift: "false",
+        isCaps: "false",
+    },
+    CapsLock: {
+        keyShiftUpEn: "CapsLock",
+        keyUpEn: "CapsLock",
+        keyShiftUpRu: "CapsLock",
+        keyUpRu: "CapsLock",
+        isShift: "false",
+        isCaps: "false",
+    },
+    KeyA: {
+        keyShiftUpEn: "A",
+        keyUpEn: "a",
+        keyShiftUpRu: "Ф",
+        keyUpRu: "ф",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyS: {
+        keyShiftUpEn: "S",
+        keyUpEn: "s",
+        keyShiftUpRu: "Ы",
+        keyUpRu: "ы",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyD: {
+        keyShiftUpEn: "D",
+        keyUpEn: "d",
+        keyShiftUpRu: "В",
+        keyUpRu: "в",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyF: {
+        keyShiftUpEn: "F",
+        keyUpEn: "f",
+        keyShiftUpRu: "А",
+        keyUpRu: "а",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyG: {
+        keyShiftUpEn: "G",
+        keyUpEn: "g",
+        keyShiftUpRu: "П",
+        keyUpRu: "п",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyH: {
+        keyShiftUpEn: "H",
+        keyUpEn: "h",
+        keyShiftUpRu: "Р",
+        keyUpRu: "р",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyJ: {
+        keyShiftUpEn: "J",
+        keyUpEn: "j",
+        keyShiftUpRu: "О",
+        keyUpRu: "о",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyK: {
+        keyShiftUpEn: "K",
+        keyUpEn: "k",
+        keyShiftUpRu: "Л",
+        keyUpRu: "л",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyL: {
+        keyShiftUpEn: "L",
+        keyUpEn: "l",
+        keyShiftUpRu: "Д",
+        keyUpRu: "д",
+        isShift: "true",
+        isCaps: "true",
+    },
+    Semicolon: {
+        keyShiftUpEn: ":",
+        keyUpEn: ";",
+        keyShiftUpRu: "Ж",
+        keyUpRu: "ж",
+        isShift: "true",
+        isCaps: "true",
+    },
+    Quote: {
+        keyShiftUpEn: "\"",
+        keyUpEn: "'",
+        keyShiftUpRu: "Э",
+        keyUpRu: "э",
+        isShift: "true",
+        isCaps: "true",
+    },
+    Enter: {
+        keyShiftUpEn: "Enter",
+        keyUpEn: "Enter",
+        keyShiftUpRu: "Enter",
+        keyUpRu: "Enter",
+        isShift: "false",
+        isCaps: "false",
+    },
+    ShiftLeft: {
+        keyShiftUpEn: "Shift",
+        keyUpEn: "Shift",
+        keyShiftUpRu: "Shift",
+        keyUpRu: "Shift",
+        isShift: "false",
+        isCaps: "false",
+    },
+    KeyZ: {
+        keyShiftUpEn: "Z",
+        keyUpEn: "z",
+        keyShiftUpRu: "Я",
+        keyUpRu: "я",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyX: {
+        keyShiftUpEn: "X",
+        keyUpEn: "x",
+        keyShiftUpRu: "Ч",
+        keyUpRu: "ч",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyC: {
+        keyShiftUpEn: "C",
+        keyUpEn: "c",
+        keyShiftUpRu: "С",
+        keyUpRu: "с",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyV: {
+        keyShiftUpEn: "V",
+        keyUpEn: "v",
+        keyShiftUpRu: "М",
+        keyUpRu: "м",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyB: {
+        keyShiftUpEn: "B",
+        keyUpEn: "b",
+        keyShiftUpRu: "И",
+        keyUpRu: "и",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyN: {
+        keyShiftUpEn: "N",
+        keyUpEn: "n",
+        keyShiftUpRu: "Т",
+        keyUpRu: "т",
+        isShift: "true",
+        isCaps: "true",
+    },
+    KeyM: {
+        keyShiftUpEn: "M",
+        keyUpEn: "m",
+        keyShiftUpRu: "Ь",
+        keyUpRu: "ь",
+        isShift: "true",
+        isCaps: "true",
+    },
+    Comma: {
+        keyShiftUpEn: "<",
+        keyUpEn: ",",
+        keyShiftUpRu: "Б",
+        keyUpRu: "б",
+        isShift: "true",
+        isCaps: "true",
+    },
+    Period: {
+        keyShiftUpEn: ">",
+        keyUpEn: ".",
+        keyShiftUpRu: "Ю",
+        keyUpRu: "ю",
+        isShift: "true",
+        isCaps: "true",
+    },
+    Slash: {
+        keyShiftUpEn: "?",
+        keyUpEn: "/",
+        keyShiftUpRu: ",",
+        keyUpRu: ".",
+        isShift: "true",
+        isCaps: "false",
+    },
+    ArrowUp: {
+        keyShiftUpEn: "▲",
+        keyUpEn: "▲",
+        keyShiftUpRu: "▲",
+        keyUpRu: "▲",
+        isShift: "false",
+        isCaps: "false",
+    },
+    ShiftRight: {
+        keyShiftUpEn: "Shift",
+        keyUpEn: "Shift",
+        keyShiftUpRu: "Shift",
+        keyUpRu: "Shift",
+        isShift: "false",
+        isCaps: "false",
+    },
+    ControlLeft: {
+        keyShiftUpEn: "Ctrl",
+        keyUpEn: "Ctrl",
+        keyShiftUpRu: "Ctrl",
+        keyUpRu: "Ctrl",
+        isShift: "false",
+        isCaps: "false",
+    },
+    MetaLeft: {
+        keyShiftUpEn: "Win",
+        keyUpEn: "Win",
+        keyShiftUpRu: "Win",
+        keyUpRu: "Win",
+        isShift: "false",
+        isCaps: "false",
+    },
+    AltLeft: {
+        keyShiftUpEn: "Alt",
+        keyUpEn: "Alt",
+        keyShiftUpRu: "Alt",
+        keyUpRu: "Alt",
+        isShift: "false",
+        isCaps: "false",
+    },
+    Space: {
+        keyShiftUpEn: "",
+        keyUpEn: "",
+        keyShiftUpRu: "",
+        keyUpRu: "",
+        isShift: "false",
+        isCaps: "false",
+    },
+    AltRight: {
+        keyShiftUpEn: "Alt",
+        keyUpEn: "Alt",
+        keyShiftUpRu: "Alt",
+        keyUpRu: "Alt",
+        isShift: "false",
+        isCaps: "false",
+    },
+    ArrowLeft: {
+        keyShiftUpEn: "◄",
+        keyUpEn: "◄",
+        keyShiftUpRu: "◄",
+        keyUpRu: "◄",
+        isShift: "false",
+        isCaps: "false",
+    },
+    ArrowDown: {
+        keyShiftUpEn: "▼",
+        keyUpEn: "▼",
+        keyShiftUpRu: "▼",
+        keyUpRu: "▼",
+        isShift: "false",
+        isCaps: "false",
+    },
+    ArrowRight: {
+        keyShiftUpEn: "▼",
+        keyUpEn: "▼",
+        keyShiftUpRu: "▼",
+        keyUpRu: "▼",
+        isShift: "false",
+        isCaps: "false",
+    },
+    ControlRight: {
+        keyShiftUpEn: "Ctrl",
+        keyUpEn: "Ctrl",
+        keyShiftUpRu: "Ctrl",
+        keyUpRu: "Ctrl",
+        isShift: "false",
+        isCaps: "false",
+    },
+}
+
 class Keyboard {
     buildingPage() {
         let wrapperEl = document.createElement("div");
         wrapperEl.classList.add("wrapper");
 
         let keyboardContainerEl = document.createElement("div");
-        keyboardContainerEl.classList.add("keyboard__container","keyboard");
+        keyboardContainerEl.classList.add("keyboard__container", "keyboard");
 
         let keyboardTitelEl = document.createElement("div");
         keyboardTitelEl.classList.add("keyboard__titel");
@@ -15,7 +530,7 @@ class Keyboard {
         keyboardTextAreaEl.setAttribute("id", "textarea");
         keyboardTextAreaEl.setAttribute("rows", "5");
         keyboardTextAreaEl.setAttribute("cols", "50");
-  
+
         let keyboardbBodyEl = document.createElement("div");
         keyboardbBodyEl.classList.add("keyboard__body");
 
@@ -32,13 +547,72 @@ class Keyboard {
         keyboardContainerEl.appendChild(keyboardTextAreaEl);
         keyboardContainerEl.appendChild(keyboardbBodyEl);
         keyboardContainerEl.appendChild(keyboardInfoEl);
-        
+
         wrapperEl.appendChild(keyboardContainerEl);
 
         document.body.appendChild(wrapperEl);
+    }
+    buildingKeyboard() {
+        let keyboardbBodyEl = document.querySelector(".keyboard__body");
+        let allKeysEl = document.createElement("div");
+        allKeysEl.classList.add("keyboard__keys");
+        for (var key in keys) {
+
+            let itemKey = document.createElement("span");
+            itemKey.classList.add("key", key);
+
+            let itemKeyRus = document.createElement("span");
+            itemKeyRus.classList.add("rus", "hidden");
+
+            let itemKeyRusDown = document.createElement("span");
+            itemKeyRusDown.classList.add("caseDown", "hidden");
+            itemKeyRusDown.textContent = keys[key].keyUpRu;
+
+            let itemKeyRusUp = document.createElement("span");
+            itemKeyRusUp.classList.add("caseUp", "hidden");
+            itemKeyRusUp.textContent = keys[key].keyShiftUpRu;
+
+            let itemKeyRusCaps = document.createElement("span");
+            itemKeyRusCaps.classList.add("caps", "hidden");
+            itemKeyRusCaps.textContent = keys[key].keyShiftUpRu;
+
+            let itemKeyRusShiftCaps = document.createElement("span");
+            itemKeyRusShiftCaps.classList.add("caps", "hidden");
+            itemKeyRusShiftCaps.textContent = keys[key].keyUpRu;
+
+            itemKeyRus.appendChild(itemKeyRusDown);
+            itemKeyRus.appendChild(itemKeyRusUp);
+            itemKeyRus.appendChild(itemKeyRusCaps);
+            itemKeyRus.appendChild(itemKeyRusShiftCaps);
+
+            let itemKeyEng = document.createElement("span");
+            itemKeyEng.classList.add("eng");
+            let itemKeyEngDown = document.createElement("span");
+            itemKeyEngDown.classList.add("caseDown");
+            itemKeyEngDown.textContent = keys[key].keyUpEn;
+            let itemKeyEngUp = document.createElement("span");
+            itemKeyEngUp.classList.add("caseUp", "hidden");
+            itemKeyEngUp.textContent = keys[key].keyShiftUpEn;
+            let itemKeyEngCaps = document.createElement("span");
+            itemKeyEngCaps.classList.add("caseUp", "hidden");
+            itemKeyEngCaps.textContent = keys[key].keyShiftUpEn;
+            let itemKeyEngShiftCaps = document.createElement("span");
+            itemKeyEngShiftCaps.classList.add("caps", "hidden");
+            itemKeyEngShiftCaps.textContent = keys[key].keyUpEn;
+            itemKeyEng.appendChild(itemKeyEngDown);
+            itemKeyEng.appendChild(itemKeyEngUp);
+            itemKeyEng.appendChild(itemKeyEngCaps);
+            itemKeyEng.appendChild(itemKeyEngShiftCaps);
+
+            itemKey.appendChild(itemKeyRus);
+            itemKey.appendChild(itemKeyEng);
+            allKeysEl.appendChild(itemKey);
+        }
+        keyboardbBodyEl.appendChild(allKeysEl);
     }
 }
 
 let myKeyboard = new Keyboard();
 myKeyboard.buildingPage();
+myKeyboard.buildingKeyboard();
 console.log("in js");
