fa3683254e7d62d509d9123f8a5ab6db301b6275
k2u6m5i3r <wunder49@yandex.by>
2023-04-30 12:03:43 +0300

    add page layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,44 @@
-const a = 1
+class Keyboard {
+    buildingPage() {
+        let wrapperEl = document.createElement("div");
+        wrapperEl.classList.add("wrapper");
 
-console.log(a);
+        let keyboardContainerEl = document.createElement("div");
+        keyboardContainerEl.classList.add("keyboard__container","keyboard");
+
+        let keyboardTitelEl = document.createElement("div");
+        keyboardTitelEl.classList.add("keyboard__titel");
+        keyboardTitelEl.textContent = "RSS Виртуальная клавиатура";
+
+        let keyboardTextAreaEl = document.createElement("textarea");
+        keyboardTextAreaEl.classList.add("keyboard__text-area");
+        keyboardTextAreaEl.setAttribute("id", "textarea");
+        keyboardTextAreaEl.setAttribute("rows", "5");
+        keyboardTextAreaEl.setAttribute("cols", "50");
+  
+        let keyboardbBodyEl = document.createElement("div");
+        keyboardbBodyEl.classList.add("keyboard__body");
+
+        let keyboardInfoEl = document.createElement("div");
+        keyboardInfoEl.classList.add("keyboard__info");
+        let keyboardInfoDesOneEl = document.createElement("p");
+        keyboardInfoDesOneEl.textContent = "Клавиатура создана в операционной системе Windows";
+        let keyboardInfoDesTwoEl = document.createElement("p");
+        keyboardInfoDesTwoEl.textContent = "Для перекоючения языка комбинация: левый crtl + alt";
+        keyboardInfoEl.appendChild(keyboardInfoDesOneEl);
+        keyboardInfoEl.appendChild(keyboardInfoDesTwoEl);
+
+        keyboardContainerEl.appendChild(keyboardTitelEl);
+        keyboardContainerEl.appendChild(keyboardTextAreaEl);
+        keyboardContainerEl.appendChild(keyboardbBodyEl);
+        keyboardContainerEl.appendChild(keyboardInfoEl);
+        
+        wrapperEl.appendChild(keyboardContainerEl);
+
+        document.body.appendChild(wrapperEl);
+    }
+}
+
+let myKeyboard = new Keyboard();
+myKeyboard.buildingPage();
+console.log("in js");
